205250ff7b94be8f63b5d7027e4d7cdf7055dd25
Isaac <isaac.cv123@gmail.com>
2026-05-11 22:26:19 -0400

    Creacion de clase Retraso

---
+++ src/main/java/org/example/Retraso.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.time.Instant;
+
+public class Retraso extends  Asistencia {
+    private Instant hora = Instant.now();
+
+    public Instant getHora() {
+        return hora;
+    }
+
+    public void setHora(Instant hora) {
+        this.hora = hora;
+    }
+
+    @Override
+    public String toString() {
+        return "Retraso:" + hora;
+    }
+}
