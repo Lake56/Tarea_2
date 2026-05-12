a047d5dc55866f574ead64d524587aa4c5eb873a
Eltigre <bastiton@gmail.com>
2026-05-12 09:59:02 -0400

    Creacion Clase Empleado

---
+++ src/main/java/Empleado.java
@@ -0,0 +1,12 @@
+class Empleado{
+    private String id;
+    private String Apellidos;
+    private String Nombre;
+    private String Correo;
+    public Empleado(String nombre, String apellidos, String correo, String id){
+        this.Nombre=nombre;
+        this.Apellidos=apellidos;
+        this.Correo=correo;
+        this.id=id;
+    }
+}
