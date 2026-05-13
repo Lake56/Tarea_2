b299a2853e3cc36f2c88e5eb9a1942a0978edac7
Eltigre <bastiton@gmail.com>
2026-05-13 13:14:54 -0400

    Mejoras en clase Departamento

--- src/main/java/Departamento.java
+++ src/main/java/Departamento.java
@@ -3,6 +3,10 @@ class Departamento{
     private int CantidadEmpleados;
     public Departamento(){
     }
+    public void AgregarEmpleado(String nombre, String apellidos, String correo, String id){
+            new Empleado(nombre,apellidos,correo,id);
+            CantidadEmpleados+=1;
+    }
     public int ObtenerCantidadEmpleados(){
         return CantidadEmpleados;
     }
