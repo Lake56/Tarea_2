af451aba06780634d066ba55924c194659433cd7
Eltigre <bastiton@gmail.com>
2026-05-14 12:38:43 -0400

    Se añaden nuevas funciones a la clase Departamento

--- src/main/java/Departamento.java
+++ src/main/java/Departamento.java
@@ -1,14 +1,16 @@
+import java.util.ArrayList;
 class Departamento{
     private String nombre;
-    private int CantidadEmpleados;
+    private ArrayList<Empleado> Empleados;
     public Departamento(){
+        Empleados=new ArrayList<>();
     }
     public Empleado AgregarEmpleado(String nombre, String apellidos, String correo, String id){
-            CantidadEmpleados+=1;
-            return new Empleado(nombre,apellidos,correo,id);
-
+        Empleado E= new Empleado(nombre,apellidos,correo,id);
+        Empleados.add(E);
+        return E;
     }
     public int ObtenerCantidadEmpleados(){
-        return CantidadEmpleados;
+        return Empleados.size();
     }
 }
