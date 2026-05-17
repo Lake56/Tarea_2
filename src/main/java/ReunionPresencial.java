1c15c83d05dd7f0e0bcb74099c03ea6f4f349f5e
mtn928 <mcarrasco2025@inf.udec.cl>
2026-05-17 01:08:23 -0400

    Getter y Setter para ReunionPresencial

--- src/main/java/ReunionPresencial.java
+++ src/main/java/ReunionPresencial.java
@@ -1,2 +1,14 @@
-public class ReunionPresencial {
+
+
+public class ReunionPresencial extends Reunion {
+
+    private String sala;
+
+    //getter y setter
+    public String getSala() {
+        return sala;
+    }
+    public void setSala(String sala) {
+        this.sala = sala;
+    }
 }
