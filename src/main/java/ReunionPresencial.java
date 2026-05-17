f95be7b1b94325bfdb18e533a73ad6f0ddb875e6
mtn928 <mcarrasco2025@inf.udec.cl>
2026-05-17 01:11:27 -0400

    Datos de la Reunion

--- src/main/java/ReunionPresencial.java
+++ src/main/java/ReunionPresencial.java
@@ -1,8 +1,15 @@
-
+import java.time.Duration;
+import java.time.Instant;
 
 public class ReunionPresencial extends Reunion {
-
     private String sala;
+
+    public ReunionPresencial(java.time.LocalDate fecha, Instant horaPrevista,
+                             Duration duracionPrevista, TipoReunion tipoReunion,
+                             Empleado organizador, String sala) {
+        super(fecha, horaPrevista, duracionPrevista, tipoReunion, organizador);
+        this.sala= sala;
+    }
 
     //getter y setter
     public String getSala() {
