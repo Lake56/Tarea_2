a0deee140ce1d2935109ff80230f48522e9d5c39
mtn928 <mcarrasco2025@inf.udec.cl>
2026-05-17 00:51:25 -0400

    Creacion atributos y constructores de Invitacion

--- src/main/java/Invitacion.java
+++ src/main/java/Invitacion.java
@@ -1,2 +1,23 @@
+import java.time.Instant;
+
 public class Invitacion {
+ //datos de la invitacion
+    private Instant hora;
+    private Invitable invitado;
+    private Reunion reunion;
+
+    //constructores
+    public Invitacion(Invitable invitado, Reunion reunion) {
+        this.hora = Instant.now(); //la hora de creacion de la invitacion
+        this.invitado = invitado;
+        this.reunion = reunion;
+    }
+
+    //constructor para declarar la hora explicitamente
+    public Invitacion(Invitable invitado, Reunion reunion, Instant hora) {
+        this.hora = hora;
+        this.invitado = invitado;
+        this.reunion = reunion;
+    }
+
 }
